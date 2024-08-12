08cc081241a12f6a2b18335e22f586d9bea5684c
nutsea <batyrova.n@icloud.com>
2024-08-12 21:03:04 +0300

    order photos multiple

--- routes/PhotoRouter.js
+++ routes/PhotoRouter.js
@@ -1,8 +1,9 @@
 const Router = require('express')
 const router = new Router()
 const photoController = require('../controllers/PhotoController')
+const adminMiddleware = require('../middleware/adminMiddleware')
 
 router.post('/', photoController.create)
-router.delete('/', photoController.delete)
+router.delete('/', adminMiddleware, photoController.delete)
 
 module.exports = router
